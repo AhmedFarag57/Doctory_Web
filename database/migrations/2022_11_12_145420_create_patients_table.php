5b3ab5b5ab28d8c7c84826bddc5e0be9e123c312
Ahmed Farag <91483335+AhmedFarag57@users.noreply.github.com>
2023-04-15 22:57:49 +0200

    Update 2022_11_12_145420_create_patients_table.php

--- database/migrations/2022_11_12_145420_create_patients_table.php
+++ database/migrations/2022_11_12_145420_create_patients_table.php
@@ -16,7 +16,7 @@ class CreatePatientsTable extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->string('fake_name')->nullable();
+            // $table->string('fake_name')->nullable();
             $table->decimal('wallet',9,3)->default(0);
             $table->timestamps();
         });
